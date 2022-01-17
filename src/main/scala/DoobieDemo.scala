0ad5e5282da5e28c3c1deaeb5655fa36f9737d8a
n4to4 <n4to4k@gmail.com>
2022-01-18 07:57:00 +0900

    wip

--- src/main/scala/DoobieDemo.scala
+++ src/main/scala/DoobieDemo.scala
@@ -1,6 +1,28 @@
 import cats.effect.{ExitCode, IO, IOApp}
+import doobie.util.transactor.Transactor
+import doobie.implicits._
 
 object DoobieDemo extends IOApp {
+  implicit class Debugger[A](io: IO[A]) {
+    def debug: IO[A] = io.map { a =>
+      println(s"[${Thread.currentThread().getName}] $a")
+      a
+    }
+  }
+
+  val xa: Transactor[IO] = Transactor.fromDriverManager[IO](
+    "org.postgresql.Driver",
+    "jdbc:postgresql:myimdb",
+    "docker",
+    "docker"
+  )
+
+  def findAllActorNames: IO[List[String]] = {
+    val query = sql"select name from actors".query[String]
+    val action = query.to[List]
+    action.transact(xa)
+  }
+
   def run(args: List[String]): IO[ExitCode] =
-    IO(println("hello")).as(ExitCode.Success)
+    findAllActorNames.debug.as(ExitCode.Success)
 }
