f393a7f712eccd066d2aaa3393bd1f88b789845c
n4to4 <n4to4k@gmail.com>
2022-01-18 16:00:03 +0900

    tagless

--- taglessfinal/src/main/scala/TaglessFinal.scala
+++ taglessfinal/src/main/scala/TaglessFinal.scala
@@ -26,28 +26,55 @@ object TaglessFinal {
         eval(a).asInstanceOf[Boolean] || eval(b).asInstanceOf[Boolean]
       // casts everywhere
     }
+  }
 
-    object Tagging {
-      trait Expr(val tag: String)
-      case class B(boolean: Boolean) extends Expr("bool")
-      case class Or(left: Expr, right: Expr) extends Expr("bool")
-      case class And(left: Expr, right: Expr) extends Expr("bool")
-      case class Not(expr: Expr) extends Expr("bool")
-      case class I(int: Int) extends Expr("int")
-      case class Sum(left: Expr, right: Expr) extends Expr("int")
+  object Tagging {
+    trait Expr(val tag: String)
+    case class B(boolean: Boolean) extends Expr("bool")
+    case class Or(left: Expr, right: Expr) extends Expr("bool")
+    case class And(left: Expr, right: Expr) extends Expr("bool")
+    case class Not(expr: Expr) extends Expr("bool")
+    case class I(int: Int) extends Expr("int")
+    case class Sum(left: Expr, right: Expr) extends Expr("int")
 
-      def eval(expr: Expr): Any = expr match {
-        case B(b) => b
-        case Or(left, right) =>
-          if (left.tag != "bool" || right.tag != "bool")
-            throw new IllegalArgumentException("improper argument type")
-          else
-            eval(left).asInstanceOf[Boolean] ||
-            eval(right).asInstanceOf[Boolean]
-        // same
-      }
+    def eval(expr: Expr): Any = expr match {
+      case B(b) => b
+      case Or(left, right) =>
+        if (left.tag != "bool" || right.tag != "bool")
+          throw new IllegalArgumentException("improper argument type")
+        else
+          eval(left).asInstanceOf[Boolean] ||
+          eval(right).asInstanceOf[Boolean]
+      // same
     }
   }
 
-  def main(args: Array[String]): Unit = {}
+  object Tagless {
+    trait Expr[A]
+    case class B(boolean: Boolean) extends Expr[Boolean]
+    case class Or(left: Expr[Boolean], right: Expr[Boolean])
+        extends Expr[Boolean]
+    case class And(left: Expr[Boolean], right: Expr[Boolean])
+        extends Expr[Boolean]
+    case class Not(expr: Expr[Boolean]) extends Expr[Boolean]
+    case class I(int: Int) extends Expr[Int]
+    case class Sum(left: Expr[Int], right: Expr[Int]) extends Expr[Int]
+
+    def eval[A](expr: Expr[A]): A = expr match {
+      case B(b)             => b
+      case I(i)             => i
+      case Or(left, right)  => eval(left) || eval(right)
+      case Sum(left, right) => eval(left) + eval(right)
+      // etc
+    }
+  }
+
+  def demoTagless(): Unit = {
+    import Tagless._
+    println(eval(Or(B(true), And(B(true), B(false)))))
+  }
+
+  def main(args: Array[String]): Unit = {
+    demoTagless()
+  }
 }
