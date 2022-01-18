16b8c22b8df68ff2ba0a568ab35da4c226c9af7d
n4to4 <n4to4k@gmail.com>
2022-01-18 16:06:13 +0900

    tagless final

--- taglessfinal/src/main/scala/TaglessFinal.scala
+++ taglessfinal/src/main/scala/TaglessFinal.scala
@@ -69,13 +69,49 @@ object TaglessFinal {
     }
   }
 
+  object TaglessFinal {
+    trait Expr[A] {
+      val value: A
+    }
+
+    def b(boolean: Boolean): Expr[Boolean] = new Expr[Boolean] {
+      val value = boolean
+    }
+
+    def i(int: Int): Expr[Int] = new Expr[Int] {
+      val value = int
+    }
+
+    def and(left: Expr[Boolean], right: Expr[Boolean]): Expr[Boolean] =
+      new Expr[Boolean] {
+        val value = left.value && right.value
+      }
+
+    def or(left: Expr[Boolean], right: Expr[Boolean]): Expr[Boolean] =
+      new Expr[Boolean] {
+        val value = left.value || right.value
+      }
+
+    def sum(left: Expr[Int], right: Expr[Int]): Expr[Int] = new Expr[Int] {
+      val value = left.value + right.value
+    }
+
+    def eval[A](expr: Expr[A]): A = expr.value
+  }
+
   def demoTagless(): Unit = {
     import TaglessInitial._
     println(eval(Or(B(true), And(B(true), B(false)))))
     println(eval(Sum(I(24), I(-3))))
   }
 
+  def demoTaglessFinal(): Unit = {
+    import TaglessFinal._
+    println(eval(or(b(true), and(b(true), b(false)))))
+    println(eval(sum(i(24), i(-3))))
+  }
+
   def main(args: Array[String]): Unit = {
-    demoTagless()
+    demoTaglessFinal()
   }
 }
